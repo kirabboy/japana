ed18c980eb07c4ccb38dba2b8ac43cc81929db72
Phan Ngọc Trí <ngocctrii47@gmail.com>
2021-09-16 15:57:20 +0700

    ngoctri

---
+++ resources/views/danhmucsanpham.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/boostrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/danhmucsanpham.css') }}">
+</head>
+
+<body>
+    <section>
+        <div class="page-title shop-page-title">
+            <div class="page-title-inner container">
+                <nav class="breadcrumbs">
+                    <a href="#">Trang chủ</a>
+                    <span class="divider">/</span>
+                    <a href="#" class="active">Chăm Sóc Sức Khỏe</a>
+                </nav>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
