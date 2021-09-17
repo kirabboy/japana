2e1b1d4e99485563fe5bdcbcf1426cf7bc8b834c
hieulam <hieulam2001@gmail.com>
2021-09-17 16:59:12 +0700

    lamhieu

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <title>Document</title>
+</head>
+
+<body>
+    @include('header')
+    @yield('content')
+    <footer class="site-footer">
+        <div class="container">
+            <div class="top-footer row">
+                <div class="info-company col-lg-6  col-md-12 col-sm-12 col-xs-12 col-12">
+                    <p class="title-info">Siêu thị Nhật Bản Japana.vn</p>
+                    <p class="des"><span><i class="fas fa-map-marker-alt"></i> CN Hồ Chí Minh:</span>
+                        Tầng trệt, Khu 15, Siêu thị Aeon Mall Tân Phú TP.HCM</p>
+                    <p class="des"><span><i class="fas fa-map-marker-alt"></i> CN Hải Phòng:</span>
+                        Tầng 1, Khu Cosmetic, Aeon Mall Hải Phòng Lê Chân, TP Hải Phòng</p>
+                    <p class="des"><span><i class="fas fa-map-marker-alt"></i> VP tại Nhật Bản:</span> Kanagawa Ken Ebina - Shi Nakana, Japan</p>
+                    <p class="des"><span><i class="fas fa-map-marker-alt"></i> VP tại Việt Nam:</span> Tòa Nhà Trường Thịnh Group, 76 Nguyễn Háo Vĩnh, P. Tân Quý, Q. Tân Phú, TP. HCM</p>
+                    <p class="des"><span><i class="fa fa-globe" aria-hidden="true"></i> Website:</span> https://japana.vn</p>
+                    <p class="des"><span><i class="fa fa-certificate" aria-hidden="true"></i> Giấy CNĐKDN:</span> 0313518985</p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.23029251409!2d106.62277041418271!3d10.79366606182825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752bff61bf4cb9%3A0x437ac5fb84a0c8cf!2zU2nDqnUgVGjhu4sgTmjhuq10IELhuqNuIE9ubGluZSAtIEphcGFuYS52bg!5e0!3m2!1svi!2s!4v1519717652110" width="95%" height="300" style="border:0" allowfullscreen=""></iframe>
+
+                </div>
+                <div class="list-support col-lg-3 col-md-12 col-sm-12 col-xs-12 col-12">
+                    <p class="title-info">Chính sách</p>
+                    <ul>
+                        <li><a rel="nofollow" href="https://japana.vn/mien-phi-van-chuyen-cskh-63-static-1.jp" title="Chính sách vận chuyển">Chính sách vận chuyển</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/dieu-khoan-giao-dich-static-2.jp" title="Điều khoản giao dịch">Điều khoản giao dịch</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/phuong-thuc-thanh-toan-static-4.jp" title="Phương thức thanh toán">Phương thức thanh toán</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/thoi-gian-giao-hang-static-5.jp" title="Thời gian giao hàng">Thời gian giao hàng</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/dieu-khoan-giao-dich-static-6.jp" title="Chính sách bảo hành">Chính sách bảo hành</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/chinh-sach-bao-mat-static-7.jp" title="Chính sách bảo mật">Chính sách bảo mật</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/chinh-sach-doi-tra-static-8.jp" title="Chính sách đổi trả và hoàn tiền">Chính sách đổi trả và hoàn tiền</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/huong-dan-mua-hang-static-9.jp" title="Hướng dẫn mua hàng">Hướng dẫn mua hàng</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/quyen-loi-vip-static-10.jp" title="Quyền lợi VIP">Quyền lợi VIP</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/quy-dinh-ban-hang-tren-website-japana-static-11.jp" title="Quy định bán hàng trên Website Japana">Quy định bán hàng trên Website Japana</a></li>
+                        <li><a rel="nofollow" href="https://japana.vn/quy-dinh-ban-hang-tren-website-japana-static-12.jp" title="Câu hỏi thường gặp">Câu hỏi thường gặp</a></li>
+                    </ul>
+                </div>
+                <div class="box-hotline-f col-lg-3 col-md-12 col-sm-12 col-xs-12 col-12">
+                    <p class="title-info">Hotline</p>
+                    <p class="hotline-f"><i class="fas fa-phone-alt"></i></i>0975 800 600 - 0935 600 800</p>
+                    <p class="title-info">Tổng đài</p>
+                    <p class="hotline-f"><i class="fas fa-phone-alt"></i></i>(028) 7108 8889</p>
+                    <p class="title-time">Giờ làm việc:</p>
+                    <p class="time-des">Từ 8h - 17h00 hàng ngày <br> (Cả thứ 7 &amp; Chủ Nhật)</p>
+
+                    <p class="title-info title-socials">Kết nối</p>
+                    <ul class="socials-f d-flex justify-content-around ">
+                        <li class="fb-icon"><a rel="nofollow" href="https://www.facebook.com/japana.sieuthinhat" title="facebook" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="fb-icon"><a rel="nofollow" href="" title="twitter" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                        <li class="fb-icon"><a rel="nofollow" href="https://www.instagram.com/japanavn/" title="instagram" target="_blank"><i class="fab fa-instagram-square"></i></a></li>
+
+                        <!--<li class="ins-icon"><a rel="nofollow" href="" title="instagram" target="_blank"><i class="ti ti-instagram"></i></a></li>
+					<li class="yt-icon"><a rel="nofollow" href="" title="youtube" target="_blank"><i class="fa fa-youtube-play"></i></a></li>-->
+                    </ul>
+                </div>
+            </div>
+
+            <div class="middle-footer row">
+
+                <div class="box-f1 col-lg-6  col-md-12 col-sm-12 col-xs-12 col-12 ">
+                    <p class="title-info">Vận chuyển</p>
+                    <ul class="d-flex">
+
+                        <li><i class="icon-desktop icon-ship1-new"></i></li>
+                        <li><i class="icon-desktop icon-ship2-new"></i></li>
+
+                    </ul>
+                </div>
+
+                <div class="box-f2 box-f2-custom col-lg-3 col-md-12 col-sm-12 col-xs-12 col-12">
+                    <p class="title-info">Chứng nhận</p>
+                    <a rel="nofollow" href="//www.dmca.com/Protection/Status.aspx?ID=d4350498-0837-4f16-a795-0da7f92f0c7b" title="DMCA.com Protection Status" class="dmca-badge">
+                        <i class="box-icon-all2 dmca-icon"></i>
+                    </a>
+
+                    <a rel="nofollow" href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=21249" title="bộ công thương">
+                        <!--                    <img src="-->
+                        <!--assets/images/bct.png" alt="Bộ công thương">-->
+
+                        <i class="icon-desktop icon-certified"></i>
+                    </a>
+                </div>
+
+                <div class="box-f3 box-f3-custom col-lg-3 col-md-12 col-sm-12 col-xs-12 col-12">
+                    <p class="title-info">Thanh toán</p>
+
+                    <i class="icon-desktop icon-payment-01"></i>
+                    <i class="icon-desktop icon-payment-02"></i>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="bottom-footer">
+            <div class="container d-flex justify-content-center ">
+                <p>© 2021-Mevivu</p>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+<style>
+    .info-company .title-info,
+    .title-info {
+        font-weight: 500;
+        color: #333;
+        font-size: 16px;
+        text-transform: uppercase;
+        margin-bottom: 19px;
+
+    }
+
+    .info-company .des {
+        padding-bottom: 9px;
+
+    }
+
+    .des {
+        line-height: 1.5;
+
+
+    }
+
+    .title-time {
+        font-family: 'Roboto';
+        font-weight: 500;
+        font-size: 14px;
+        color: #333;
+        margin-top: 20px;
+    }
+
+    .info-company p span {
+        color: #333;
+
+        font-weight: 500;
+    }
+
+    .info-company i {
+        margin-right: 4px;
+        width: 14px;
+    }
+
+    .info-company .des,
+    .info-company .des span,
+    .list-support ul li a,
+    .box-hotline-f .time-des,
+    .box-hotline-f .title-time {
+        color: #608293 !important;
+    }
+
+    .list-support ul li {
+        padding: 0 0 17px;
+    }
+
+    ul,
+    ol {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .time-des {
+        font-family: 'Roboto';
+        font-weight: 400;
+        font-size: 14px;
+        color: #666;
+        margin-top: 8px;
+        line-height: 1.5;
+    }
+
+    .hotline-f {
+
+        font-size: 16px;
+        font-weight: 500;
+        color: #9e1836 !important;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        margin-bottom: 15px;
+    }
+
+    .fas.fa-phone-alt {
+        padding-right: 17px;
+    }
+    .socials-f{
+        width: 55%;
+    }
+    .socials-f li a i{
+        font-size:30px;
+        color: #222
+    }
+    .site-footer {
+    margin-top: 30px;
+    background: #f9f9f9;
+}
+.icon-ship1-new {
+    display: inline-block;
+    background-position: -34px -715px!important;
+    width: 110px!important;
+    height: 65px!important;
+    position: relative;
+    top: -30px;
+}
+.icon-all-new, .icon-desktop {
+    background: url(https://japana.vn/assets/frontend/assets/images/icons_new.png) no-repeat;
+}
+.icon-ship2-new {
+    display: inline-block;
+    background-position: -12px -779px!important;
+    width: 150px!important;
+    height: 70px!important;
+    position: relative;
+    top: -30px;
+}
+.dmca-icon {
+    background-position: 0 0;
+    width: 55px;
+    height: 56px;
+    display: inline-block;
+}
+.box-icon-all2 {
+    background: url(https://japana.vn/assets/images/icon-pc2.png) no-repeat;
+}
+.icon-certified {
+    display: inline-block;
+    background-position: -17px -533px!important;
+    width: 180px!important;
+    height: 65px!important;
+    position: relative;
+    top: -30px;
+}
+
+
+.icon-payment-01 {
+    display: inline-block;
+    background-position: -34px -666px!important;
+    width: 110px!important;
+    height: 75px!important;
+    position: relative;
+    top: -25px;
+    left: -22px;
+}
+.icon-all-new, .icon-desktop {
+    background: url(https://japana.vn/assets/frontend/assets/images/icons_new.png) no-repeat;
+}
+.icon-payment-02 {
+    display: inline-block;
+    background-position: -34px -608px!important;
+    width: 110px!important;
+    height: 75px!important;
+    position: relative;
+    top: -25px;
+    left: -72px;
+}
+.middle-footer{
+    padding: 20px 0;
+}
+
+</style>
