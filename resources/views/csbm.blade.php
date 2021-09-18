08b652b10e00e08dcc31346e0ad3ff474c9238b6
DaiPhuc <phamdaiphuc001@gmail.com>
2021-09-18 11:44:43 +0700

    them bai vao

--- resources/views/csbm.blade.php
+++ resources/views/csbm.blade.php
@@ -150,10 +150,10 @@
           <div class="nav flex-column">
             <a href="#"  class=" content-hover nav-item nav-link"> Japana Việt Nam</a>
             <a href="#"  class="active-csv content-hover nav-item nav-link"> Chăm sóc khách hàng</a>
-            <a href="#"  class=" content-hover nav-item nav-link">Chính sách vận chuyển</a>
-            <a href="#"  class=" content-hover nav-item nav-link">Điều khoản giao dịch</a>
-            <a href="#"  class=" content-hover nav-item nav-link">Phương thức thanh toán</a>
-            <a href="#"  class=" content-hover nav-item nav-link">Thời gian giao hàng </a>
+            <a href="{{url('/')}}"  class=" content-hover nav-item nav-link">Chính sách vận chuyển</a>
+            <a href="{{url('/dkgd')}}"  class=" content-hover nav-item nav-link">Điều khoản giao dịch</a>
+            <a href="{{url('/pttt')}}"  class=" content-hover nav-item nav-link">Phương thức thanh toán</a>
+            <a href="{{url('/')}}"  class=" content-hover nav-item nav-link">Thời gian giao hàng </a>
             <a href="#"  class=" content-hover nav-item nav-link">Chính sách bảo hành </a>
             <a href="#"  class=" content-hover nav-item nav-link">Chính sách đổi trả và hoàn tiền </a>
             <a href="#"  class="  content-hover nav-item nav-link">Hướng dẫn mua hàng  </a>
