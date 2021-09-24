baa74a0222dd8722092b68e2700e5017963b9cd3
unknown <vuquocminh99@gmail.com>
2021-09-24 14:31:00 +0700

    update admin interface

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,44 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+
+// ADMIN
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    });
+    Route::get('/login', function () {
+        return view('admin.login');
+    });
+    Route::get('/don-hang', function () {
+        return view('admin.don-hang');
+    });
+    Route::get('/don-vi-tinh', function () {
+        return view('admin.don-vi-tinh');
+    });
+    Route::get('/danh-muc-san-pham', function () {
+        return view('admin.productCategory');
+    });
+    Route::get('/san-pham', function () {
+        return view('admin.product');
+    });
+    Route::get('/phan-quyen', function () {
+        return view('admin.phan-quyen');
+    });
+    Route::get('/profile', function () {
+        return view('admin.profile');
+    });
+    Route::get('/setting', function () {
+        return view('admin.setting');
+    });
+    Route::get('/ton-kho', function () {
+        return view('admin.ton-kho');
+    });
+});
+// END ADMIN
+
+
 Route::get('/product', [ProductController::class, 'product']);
 Route::get('/danh-muc-san-pham', [ProductCategoryController::class, 'getdanhmucsanpham']);
 
