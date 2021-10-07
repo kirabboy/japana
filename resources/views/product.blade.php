0dcee55456f6cffc414510ee7c8eb75ac8152e6a
Your Name <you@example.com>
2021-10-07 14:37:00 +0700

    'duyphuc'

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,7 +5,7 @@
 @push('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('css/boostrap/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/product-detail.css')}}">
     <link rel="stylesheet" href="{{ asset('css/fotorama.css')}}">
 @endpush
 
